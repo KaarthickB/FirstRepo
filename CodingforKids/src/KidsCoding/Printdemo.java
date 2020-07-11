bc3143564a3221265f111961160564cbd3ec420f
Gaya3Balasubramanian <Gaya3Balasubramanian@gmail.com>
2020-07-11 19:34:45 +0530

    CodingforKids

---
+++ CodingforKids/src/KidsCoding/Printdemo.java
@@ -0,0 +1,10 @@
+package KidsCoding;
+
+public class Printdemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ CodingforKids/src/KidsCoding/Scannerdemo.java
@@ -0,0 +1,15 @@
+package KidsCoding;
+
+import java.util.Scanner;
+
+public class Scannerdemo {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		String x= scan.nextLine();
+		System.out.println(x);
+		
+
+	}
+
+}
